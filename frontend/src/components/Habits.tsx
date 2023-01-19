42b8dae979203f5acd9053c6c1be611ea6390b94
AlexWasHeree <victoralexandre2303@gmail.com>
2023-01-19 13:20:18 -0300

    first commit

---
+++ frontend/src/components/Habits.tsx
@@ -0,0 +1,11 @@
+// interface HabitsProps {
+//   completed: number
+// }
+
+export function Habits() {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
+      {/* {props.completed} */}
+    </div>
+  );
+}
